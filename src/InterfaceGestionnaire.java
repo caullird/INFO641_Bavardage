181e70deca03e298d97de390cd5c901447882918
CAULLIREAU Dorian <caullireau.dorian@gmail.com>
2021-05-11 16:32:40 +0200

    Update InterfaceGestionnaire.java

--- src/InterfaceGestionnaire.java
+++ src/InterfaceGestionnaire.java
@@ -10,20 +10,19 @@ import java.util.ArrayList;
 public class InterfaceGestionnaire extends JFrame implements ActionListener {
     private Concierge concierge;
 
-    private JPanel panel1 = new JPanel();
-    private JPanel panel2 = new JPanel();
-
-    private JLabel labelDiscussion = new JLabel("Liste des discussions :");
-    private JLabel labelConnectes = new JLabel("Bavards connectés :");
-    private JLabel labelNom = new JLabel("Nom du bavard :");
+    private JPanel listernerPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+    private JLabel listernerChatLabel = new JLabel("Liste des discussions :");
+    private JLabel listernerOnlineLabel = new JLabel("Bavards connectés :");
 
     private String message = "";
     private JTextField corps = new JTextField("",15);
 
     private JTextPane zoneMessages = new JTextPane();
-    JEditorPane jEditorPane = new JEditorPane();
+    private JTextPane connectedList = new JTextPane();
     HTMLEditorKit kit = new HTMLEditorKit();
 
+    private JPanel formPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+    private JLabel labelNom = new JLabel("Nom du bavard :");
     private JButton boutonNewb = new JButton("Ajouter un bavard");
 
     public InterfaceGestionnaire(Concierge concierge){
@@ -36,77 +35,53 @@ public class InterfaceGestionnaire extends JFrame implements ActionListener {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // HTML
-        jEditorPane.setEditable(false);
-        JScrollPane scrollPane = new JScrollPane(jEditorPane);
-        jEditorPane.setEditorKit(kit);
+        JScrollPane scrollPaneOnline = new JScrollPane(connectedList);
+        connectedList.setPreferredSize(new Dimension(250, 185));
+        connectedList.setEditable(false);
+        connectedList.setEditorKit(kit);
+        connectedList.setText("<font color=red>Il n'y a pas encore d'utilisateur connectés</font>");
 
-        jEditorPane.setText("<font color=red>Il n'y a pas encore d'utilisateur</font>");
+        // Mise en place de formPanel
+        formPanel.add(labelNom);
+        formPanel.add(corps);
+        formPanel.add(boutonNewb);
+        formPanel.setVisible(true);
+
+        // Creation du container
+        Container mainContainer = this.getContentPane();
+        System.out.println(mainContainer);
+        mainContainer.add(listernerPanel);
+
+        // Creation des bordures
+        Border border1 =  BorderFactory.createEmptyBorder(30,35,30,35);
+        Border border2 =  BorderFactory.createEmptyBorder(30,35,30,35);
+
+        // Creation du Layout
+        BoxLayout layout = new BoxLayout(listernerPanel,BoxLayout.Y_AXIS);
+
+        // Creation de la zone Messages
+        JScrollPane scrollPaneMessage = new JScrollPane(this.zoneMessages);
+        zoneMessages.setEditable(false);
+        zoneMessages.setEditorKit(kit);
+        zoneMessages.setPreferredSize(new Dimension(250, 185));
+
+        // Mise en place de listernerPanel
+        listernerPanel.setBorder(border1);
+        listernerPanel.setLayout(layout);
+        listernerPanel.add(listernerChatLabel);
+        listernerPanel.add(scrollPaneMessage);
+        listernerPanel.add(listernerOnlineLabel);
+        listernerPanel.add(scrollPaneOnline);
+        listernerPanel.add(formPanel);
 
         // Ajout des boutons comme ecouteurs
         boutonNewb.addActionListener(this);
         boutonNewb.setActionCommand("createBavard");
 
-        // Mise en place de panel2
-        panel2.add(labelNom);
-        panel2.add(corps);
-        panel2.add(boutonNewb);
-        panel2.setVisible(true);
-
-        // Creation du container
-        Container mainContainer = this.getContentPane();
-        System.out.println(mainContainer);
-        mainContainer.add(panel1);
-
-        // Creation des bordures
-        Border border1 =  BorderFactory.createEmptyBorder(30,35,30,35);
-        Border border2 =  BorderFactory.createEmptyBorder(20,45,20,45);
-
-        // Creation du Layout
-        BoxLayout layout = new BoxLayout(panel1,BoxLayout.Y_AXIS);
-
-        // Mise en place des labels
-        Font font = new Font("Helvetica",Font.BOLD,24);
-        Font font2 = new Font("Helvetica",Font.BOLD,18);
-
-        labelDiscussion.setFont(font);
-        labelConnectes.setFont(font);
-        labelNom.setFont(font2);
-
-        labelDiscussion.setForeground(Color.black);
-        labelConnectes.setForeground(Color.black);
-        labelNom.setForeground(Color.black);
-
-        // Creation de la zone Messages
-        this.zoneMessages.setEditable(false);
-        this.zoneMessages.setEditorKit(kit);
-        JScrollPane scrollPane2 = new JScrollPane(this.zoneMessages);
-        this.zoneMessages.setBorder(border2);
-        this.zoneMessages.setPreferredSize(new Dimension(250, 185));
-        this.zoneMessages.setMinimumSize(new Dimension(10, 10));
-
-        // Centrage des elements
-        labelDiscussion.setAlignmentX(Component.CENTER_ALIGNMENT);
-        this.zoneMessages.setAlignmentX(Component.CENTER_ALIGNMENT);
-        scrollPane.setAlignmentX(Component.CENTER_ALIGNMENT);
-        scrollPane2.setAlignmentX(Component.CENTER_ALIGNMENT);
-        boutonNewb.setAlignmentX(Component.CENTER_ALIGNMENT);
-        labelConnectes.setAlignmentX(Component.CENTER_ALIGNMENT);
-        labelNom.setAlignmentX(Component.CENTER_ALIGNMENT);
-        corps.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-        // Mise en place de panel1
-        panel1.setBorder(border1);
-        panel1.setLayout(layout);
-        panel1.add(labelDiscussion);
-        panel1.add(scrollPane2);
-        panel1.add(labelConnectes);
-        panel1.add(scrollPane);
-        panel1.add(scrollPane, BorderLayout.CENTER);
-        panel1.add(panel2);
-
+        InterfaceRegister iR = new InterfaceRegister();
+        iR.setConcierge(concierge);
+        iR.setiG(this);
         concierge.setInterfaceGestionnaire(this);
-
-        // Creation de l'interface connection et on relie le concierge et l'interface du gestionnaire à iC
 
         pack();
         setVisible(true);
@@ -115,18 +90,42 @@ public class InterfaceGestionnaire extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getActionCommand() == "createBavard") {
-            this.concierge.newBavard(corps.getText());
+            String nameBavard = corps.getText();
+
+            if(!nameBavard.isEmpty()){
+                Bavard bavard = concierge.newBavard(nameBavard);
+                InterfaceBavard iB = new InterfaceBavard(bavard);
+                iB.setBavard(bavard);
+                iB.setConcierge(concierge);
+                bavard.setiB(iB);
+            }
+
             corps.setText("");
         }
     }
 
-    public void updateConnectedBavard(ArrayList<Bavard> bavards) {
-        ArrayList<String> connectedBavad = new ArrayList<>();
-        for (Bavard bavard : bavards) {
+    public void displayOnlineUser() {
+        String charString = "";
+        System.out.println(concierge.getPapotageListeners());
+        for (PapotageListener bavard : concierge.getPapotageListeners()) {
             if (bavard.getConnected()) {
-                connectedBavad.add(bavard.getName());
+                charString +=  bavard.getName() + " : " + "<font color=green>est maintenant en ligne ! </font> <br/>";
+            }else {
+                charString +=  bavard.getName() + " : " + "<font color=red>est hors-ligne</font> <br/>";
             }
         }
-        this.jEditorPane.setText(String.join(", ",connectedBavad));
+        connectedList.setText(charString);
+    }
+
+    public void displayMessage(PapotageEvent mess, PapotageListener envoyeur, PapotageListener destinataire) {
+        String charString = "";
+        charString += this.message = this.message +
+                "Message de " + envoyeur.getName()+ " à </b>" + destinataire.getName() + "<br/>"+
+                "<b>Sujet : </b>" + mess.getSubject() + "<br/> " + mess.getBody() + "<br/>";
+        zoneMessages.setText(charString);
+    }
+
+    public Concierge getConcierge() {
+        return concierge;
     }
 }
