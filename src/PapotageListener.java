e64c78099e5976cc7873429ac7563bd3d97ed35c
CAULLIREAU Dorian <caullireau.dorian@gmail.com>
2021-05-18 10:48:41 +0200

    Update PapotageListener.java

--- src/PapotageListener.java
+++ src/PapotageListener.java
@@ -3,7 +3,9 @@ public interface PapotageListener {
     public String getName();
     public InterfaceBavard getiB();
     public void setiB(InterfaceBavard iB);
-    public void banUser(String command);
+
 
     public Boolean getConnected();
+
+    void responsePrivateMessage(String text, String text1, PapotageListener requestor, PapotageListener receiver, InterfacePrivateMessage interfacePrivateMessage, InterfacePrivateMessage interfaceMPReceiver);
 }
